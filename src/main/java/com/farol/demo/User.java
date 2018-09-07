eb65a2cc00172a2296c56e011a60ee6deb8b75d3
lopolafa <brandonburgoafigueroa@gmail.com>
2018-09-06 20:00:04 -0400

    init

---
+++ src/main/java/com/farol/demo/User.java
@@ -0,0 +1,10 @@
+package com.farol.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int Id;
+}
---
+++ src/main/java/com/farol/demo/UserRepo.java
@@ -0,0 +1,6 @@
+package com.farol.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+}
